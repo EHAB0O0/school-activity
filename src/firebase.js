1184ca63892c9e04e45ea0a82538072d5ca511ed
EHAB <ehab@example.com>
2026-01-20 14:34:24 +0300

    Implement Environment Variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLVdLeyqamJJEaYMTKlzLOOVAE3QhyK3g",
-  authDomain: "school-activity-manageme-7c78f.firebaseapp.com",
-  projectId: "school-activity-manageme-7c78f",
-  storageBucket: "school-activity-manageme-7c78f.firebasestorage.app",
-  messagingSenderId: "989397764336",
-  appId: "1:989397764336:web:b4565c45e3fbf6e16e1a35",
-  measurementId: "G-5QXHD7XDHP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
